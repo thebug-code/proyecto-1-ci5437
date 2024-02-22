ce20d46732436fe1b2fa85ec667f8837cf2baf9b
Alejandro Meneses <83224454+thebug-code@users.noreply.github.com>
2024-02-22 19:50:48 -0400

    .sh rubic

--- problems/rubikscube/PDB/puzzlesPDB.sh
+++ problems/rubikscube/PDB/puzzlesPDB.sh
@@ -4,7 +4,7 @@ make -f makePDB.mk rubikscube3x3x3/8corners3coloursB.pdb
 make -f makePDB.mk rubikscube3x3x3/8corners3coloursG.pdb
 make -f makePDB.mk rubikscube3x3x3/8corners3coloursR.pdb
 make -f makePDB.mk rubikscube3x3x3/8corners3coloursY.pdb
-make -f makePDB.mk rubikscube3x3x3/12edges2coloursB.pdb
-make -f makePDB.mk rubikscube3x3x3/12edges2coloursG.pdb
-make -f makePDB.mk rubikscube3x3x3/12edges2coloursR.pdb
-make -f makePDB.mk rubikscube3x3x3/12edges2coloursY.pdb
+make -f makePDB.mk rubikscube3x3x3/12edges3coloursB.pdb
+make -f makePDB.mk rubikscube3x3x3/12edges3coloursG.pdb
+make -f makePDB.mk rubikscube3x3x3/12edges3coloursR.pdb
+make -f makePDB.mk rubikscube3x3x3/12edges3coloursY.pdb
